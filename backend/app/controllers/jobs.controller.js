47097f64341e0f79df59c9b2ea08ba3785073800
Vishal Karmakar <karmakarvishal486@gmail.com>
2023-07-16 16:30:53 -0400

    #37 CRUD API Done for Job Postings

---
+++ backend/app/controllers/jobs.controller.js
@@ -0,0 +1,83 @@
+const { where } = require("sequelize");
+const db = require("../models");
+
+exports.createOrUpdate = async (req, res) => {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const id = req.payload.id;
+        const checkJob = await db.job.findOne({
+            where: {
+              id: id
+            },
+          });
+        
+        let job = null;
+        // Update
+        if (checkJob != null) {
+            job = await db.job.update(
+                {
+                    type: req.body.type,
+                    title: req.body.title,  
+                    description: req.body.description,  
+                    updated_at: req.body.updated_at,  
+                    location: req.body.location
+                },
+                { where: { id: id }, individualHooks: true },
+                { transaction }
+            );
+            job = {
+                id: checkJob.id,
+                type: req.body.type,
+                title: req.body.title,  
+                description: req.body.description,  
+                updated_at: new Date(),  
+                location: req.body.location
+            };
+        } else {
+            // Create
+            job = await db.job.create(
+                {
+                    type: req.body.type,
+                    title: req.body.title,  
+                    description: req.body.description,  
+                    created_at: new Date(),  
+                    location: req.body.location
+                },
+                { transaction }
+            );
+        }
+        await transaction.commit()
+        res.status(200).json(job);
+    } catch (e) {
+        console.log(e);
+        await transaction.rollback();
+        res.status(500).json({ statusText: "Internal server error occured" });
+    }
+};
+
+exports.deleteJobs = async(req,res)=>{
+    const transaction = await db.sequelize.transaction();
+    try {
+        const id = req.payload.id;
+        const checkJob = await db.job.findOne({
+            where: {
+              id: id
+            },
+          });
+        
+        if (checkJob != null) {
+            await db.job.Destroy(
+                { where: { id: id }, individualHooks: true },
+                { transaction }
+            );
+        } else {
+            res.status(200).json("No Job Found");
+        }
+        await transaction.commit()
+        res.status(200).json("Job is now deleted");
+    } catch (e) {
+        console.log(e);
+        await transaction.rollback();
+        res.status(500).json({ statusText: "Internal server error occured" });
+    }
+}
---
+++ backend/app/routes/jobs.routes.js
@@ -0,0 +1,53 @@
+const jobsController = require("../controllers/jobs.controller");
+const { check, validationResult } = require("express-validator");
+const auth = require("../middlewares/auth");
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+        );
+
+        next();
+    });
+    app.use(auth.verifyToken);
+
+    // The 405 handler
+    const methodNotAllowed = (req, res, next) =>
+        res.status(405).json({ statusText: "Method not supported" });
+
+    const createUpdateJobsRoute = "/api/jobs";
+    app.put(
+        createUpdateJobsRoute,
+      check("type").notEmpty().withMessage("type is required."),
+      check("title").notEmpty().withMessage("title is required."),
+      check("description").notEmpty().withMessage("description is required."),
+      check("location").notEmpty().withMessage("location is required."),
+      
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(406).json({ statusText: errors.array()[0]?.msg });
+        }
+        next();
+      },
+      jobsController.createOrUpdate
+    );
+
+    const deleteJobs = "/api/jobs"
+    app.delete(
+        deleteJobsAPI,
+        check("id").notEmpty().withMessage("id is required"),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+              return res.status(406).json({ statusText: errors.array()[0]?.msg });
+            }
+            next();
+          },
+        jobsController.deleteJobs
+
+    )
+    app.all(createUpdateJobsRoute, methodNotAllowed);
+};
