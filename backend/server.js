b6e7f1ac632a66e7b8d9e3a7af4102ecdfb3a0f7
Jay Dhanani <jaydhanani99.jd@gmail.com>
2023-07-31 17:55:46 -0400

    Fixed CORS error

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,26 @@ const config = require("./app/config/config.js");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "*",
-};
+// Add headers
+app.use(function (req, res, next) {
 
-app.use(cors(corsOptions));
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
